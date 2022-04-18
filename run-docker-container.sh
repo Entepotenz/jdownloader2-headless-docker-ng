a752e322fe40bd48ebc22491404c87b3e94aac04
Florian Kretschmer <opens@posteo.de>
2022-04-18 13:58:00 +0200

    update docs

--- run-docker-container.sh
+++ run-docker-container.sh
@@ -8,10 +8,10 @@ PASSWORD="SECRET"
 
 # docker run --rm --name jd2 \
 #     -e EMAIL="${EMAIL}" -e PASSWORD="${PASSWORD}" \
-#     "${PROJECT_NAME}:latest"
+#     "ghcr.io/${PROJECT_NAME}:latest"
 
 docker run --rm --name jd2 \
     -e EMAIL="${EMAIL}" -e PASSWORD="${PASSWORD}" \
     -v /config/jd2:/opt/JDownloader/cfg \
     -v "${HOME}/Downloads":/opt/JDownloader/Downloads \
-    "${PROJECT_NAME}:latest"
+    "ghcr.io/${PROJECT_NAME}:latest"
