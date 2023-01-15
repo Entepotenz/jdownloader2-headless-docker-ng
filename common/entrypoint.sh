dc8ed522a7a32179a17c492cca72670f1e445c50
F.K <19738301+Entepotenz@users.noreply.github.com>
2023-01-15 11:28:34 +0100

    fix linter complaints

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -11,7 +11,7 @@ if [ -z "$UID" ]; then UID=1000; fi
 
 if [ "$GID" -ne "0" ]; then
 	GROUP=jdownloader
-	groupadd -g $GID $GROUP
+	groupadd -g "$GID" "$GROUP"
 else
 	GROUP=root
 fi
@@ -20,26 +20,26 @@ if [ "$UID" -ne "0" ]; then
     USER=jdownloader
 
     # Create user without home (-M) and remove login shell
-    useradd -M -s /bin/false -g $GID -u $UID $USER
+    useradd -M -s /bin/false -g "$GID" -u "$UID" "$USER"
 else
     USER=root
-    usermod -ag $GID
+    usermod -ag "$GID"
 fi
 
 # Set MyJDownloader credentials
 CONFIG_FILE="/opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
-if [ ! -z "$EMAIL" ] ; then
+if [ -n "$EMAIL" ] ; then
     if [ ! -f "$CONFIG_FILE" ] || [ ! -s "$CONFIG_FILE" ] ; then
         echo '{}' > "$CONFIG_FILE"
     fi
 
     CFG=$(jq -r --arg EMAIL "$EMAIL" --arg PASSWORD "$PASSWORD" '.email = $EMAIL | .password = $PASSWORD' "$CONFIG_FILE")
-    [ ! -z "$CFG" ] && echo "$CFG" > "$CONFIG_FILE"
+    [ -n "$CFG" ] && echo "$CFG" > "$CONFIG_FILE"
 fi
 
-chown -R $UID:$GID /opt/JDownloader
+chown -R "$UID:$GID" /opt/JDownloader
 
-su-exec ${UID}:${GID} "$@"
+su-exec "${UID}:${GID}" "$@"
 
 # Keep container alive when jd2 restarts
 while sleep 3600; do :; done
