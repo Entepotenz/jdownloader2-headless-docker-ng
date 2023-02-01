bfd04b961b9a3519864c6ae657792c0f6d44e3d0
F.K <19738301+Entepotenz@users.noreply.github.com>
2023-02-01 18:52:21 +0100

    fix entrypoint shell

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -6,8 +6,8 @@ set -o pipefail
 if [ "${TRACE-0}" -eq 1 ]; then set -o xtrace; fi
 
 # Set defaults for uid and gid to not be root
-if [ -z "$GID" ]; then GID=100;  fi
-if [ -z "$UID" ]; then UID=1000; fi
+if [ -z "${GID}" ]; then GID=100;  fi
+if [ -z "${UID}" ]; then UID=1000; fi
 
 if [ "$GID" -ne "0" ]; then
     GROUP=jdownloader
@@ -31,7 +31,7 @@ fi
 
 # Set MyJDownloader credentials
 CONFIG_FILE="/opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
-if [ -n "$EMAIL" ] ; then
+if [ -n "${EMAIL}" ] ; then
     if [ ! -f "$CONFIG_FILE" ] || [ ! -s "$CONFIG_FILE" ] ; then
         echo '{}' > "$CONFIG_FILE"
     fi
@@ -42,7 +42,7 @@ fi
 
 chown -R "$UID:$GID" /opt/JDownloader
 
-su-exec "${UID}:${GID}" "$@"
+su-exec "$UID:$GID" "$@"
 
 # Keep container alive when jd2 restarts
 while sleep 3600; do :; done
